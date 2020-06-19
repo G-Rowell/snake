a4a2903510ad8a0acf3c89d248940efec75d9fc7
Galen Rowell <growell3@gmail.com>
2020-06-19 14:10:59 +1000

    Started major refactor

--- src/main.py
+++ src/main.py
@@ -4,36 +4,68 @@
 import qtSub
 
 ##Library imports
-from PyQt5.QtWidgets import QApplication, QGridLayout, QWidget
-#from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget
-#from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 ###############################################################################
 
-class Board():
-    def __init__(self, rows, cols):
-        self.widget = QWidget()
-        self.layout = QGridLayout()
+BOARD_SIZE_X = 40
+BOARD_SIZE_Y = 40
+
+SNAKE_LENGTH = 6
+
+###############################################################################
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("QT Snake")
+
+        self.map = QWidget()
+        self.mapLayout = QGridLayout()
+        self.mapLayout.setSpacing(3)
+        self.map.setLayout(self.mapLayout)
+        
+
+
+        self.initMap()
+
+        self.setCentralWidget(self.map)
+
+        self.show()
+
+    def initMap(self):
         self.list = []
 
-        for x in range(rows):
+        for x in range(BOARD_SIZE_X):
             tempList = [] 
-            for y in range(cols):
-                currWidget = qtSub.Color('red')
+            for y in range(BOARD_SIZE_Y):
+                if x % 2 == 0:
+                    currWidget = Color('Red')
+                else:
+                    currWidget = Color('gray')
                 tempList.append(currWidget)
-                self.layout.addWidget(currWidget,x,y)
+                self.mapLayout.addWidget(currWidget,x,y)
             self.list.append(tempList) 
 
-        self.widget.setLayout(self.layout)
+class Color(QWidget):
+    def __init__(self, color, *args, **kwargs):
+        super(Color, self).__init__(*args, **kwargs)
+        self.setAutoFillBackground(True)
+        
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
 
-app = QApplication([])
 
-window = qtSub.MainWindow()
-myBoard = Board(5,5)
-window.setCentralWidget(myBoard.widget)  #TODO
-window.show()
-
-# Start the event loop
-app.exec_()
+if __name__ == '__main__':
+    app = QApplication([])
+    window = MainWindow()
+    
+    # Start the event loop
+    app.exec_()
 
         
+
